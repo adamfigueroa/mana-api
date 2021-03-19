aab9448d650da4438d575592c775d6663e9cd8fa
Adam Figueroa <admiralfiggins@gmail.com>
2021-03-19 12:28:48 -0700

    adjusted migration 001

--- migrations/002.do.create_user_practice.sql
+++ migrations/002.do.create_user_practice.sql
@@ -2,7 +2,10 @@ CREATE TABLE user_practice (
     id SERIAL PRIMARY KEY,
     practice_name TEXT NOT NULL,
     days_to_track INTEGER NOT NULL,
+    days_left INTEGER NOT NULL,
     date_start TIMESTAMPTZ NOT NULL DEFAULT now(),
+    dates_complete DATE[],
+    dates_incomplete DATE[],
     day_of_week TEXT[],
     user_id INTEGER
     REFERENCES mana_users(id) ON DELETE CASCADE NOT NULL
