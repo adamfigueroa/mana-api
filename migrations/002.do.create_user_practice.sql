ddac3bbb7f6a01b8a3ac833f48c714ac17512849
Adam Figueroa <admiralfiggins@gmail.com>
2021-03-28 17:06:06 -0700

    removed redundant column: days_left

--- migrations/002.do.create_user_practice.sql
+++ migrations/002.do.create_user_practice.sql
@@ -2,7 +2,6 @@ CREATE TABLE user_practice (
     id SERIAL PRIMARY KEY,
     practice_name TEXT NOT NULL,
     days_to_track INTEGER NOT NULL,
-    days_left INTEGER NOT NULL,
     date_start TIMESTAMPTZ NOT NULL DEFAULT now(),
     dates_complete DATE[],
     dates_incomplete DATE[],
